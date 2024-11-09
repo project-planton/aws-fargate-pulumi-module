7dddc7c3f9344017ff290c1a3dd079ac13814c35
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-11-09 17:58:31 +0530

    switched over to using local generated stubs for project-planton apis

--- pkg/main.go
+++ pkg/main.go
@@ -1,8 +1,8 @@
 package pkg
 
 import (
-	awsfargatev1 "buf.build/gen/go/project-planton/apis/protocolbuffers/go/project/planton/provider/aws/awsfargate/v1"
 	"github.com/pkg/errors"
+	awsfargatev1 "github.com/project-planton/project-planton/apis/go/project/planton/provider/aws/awsfargate/v1"
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
